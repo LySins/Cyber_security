f80f1f5f10608306c8c7f467dc58a24552cffd20
LySins <sinsoff.regural@gmail.com>
2023-02-18 20:46:20 +0700

    Disigned controller right-left by touch scree

--- Assets/Scripts/Player_controller.cs
+++ Assets/Scripts/Player_controller.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using UnityEngine.tvOS;
 
 public class Player_controller : MonoBehaviour
 {
@@ -18,17 +19,18 @@ public class Player_controller : MonoBehaviour
     void Update()
     {
 
-        //_playerTransform.position += new Vector3(0, 1 * Time.deltaTime * _playerSpeed,0);
+        
         //
         #if UNITY_ANDROID
         if(Input.touchCount > 0) 
         {
             _touch = Input.GetTouch(0);
-            if (_touch.position.x > _triggerPosition.x)
+            if (_touch.position.x >  Screen.width/2)
+            {
                 _playerTransform.position +=
                 new Vector3(1 * Time.deltaTime * _playerSpeed, 0, 0);
-
-            else if(_touch.position.x < _triggerPosition.x)
+            }
+            else if (_touch.position.x < Screen.width / 2)
                 _playerTransform.position +=
                 new Vector3(-1 * Time.deltaTime * _playerSpeed, 0, 0);
         }
