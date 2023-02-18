0d1ffe0d9c25a7143eaa93ae6de66462698e2525
LySins <sinsoff.regural@gmail.com>
2023-02-18 19:26:16 +0700

    Added controller to player

--- Assets/Scripts/Player_controller.cs
+++ Assets/Scripts/Player_controller.cs
@@ -6,6 +6,9 @@ public class Player_controller : MonoBehaviour
 {
     public float _playerSpeed = 2.0f;
     public Transform _playerTransform;
+    private Touch _touch;
+    private Vector2 _triggerPosition = new Vector2(0,0);
+
     void Start()
     {
         
@@ -14,7 +17,25 @@ public class Player_controller : MonoBehaviour
 
     void Update()
     {
-        _playerTransform.position += new Vector3(0, 1 * Time.deltaTime * _playerSpeed,0);
-        
+
+        //_playerTransform.position += new Vector3(0, 1 * Time.deltaTime * _playerSpeed,0);
+        //
+        #if UNITY_ANDROID
+        if(Input.touchCount > 0) 
+        {
+            _touch = Input.GetTouch(0);
+            if (_touch.position.x > _triggerPosition.x)
+                _playerTransform.position +=
+                new Vector3(1 * Time.deltaTime * _playerSpeed, 0, 0);
+
+            else if(_touch.position.x < _triggerPosition.x)
+                _playerTransform.position +=
+                new Vector3(-1 * Time.deltaTime * _playerSpeed, 0, 0);
+        }
+
+
+
+
+        #endif
     }
 }
